c561d97db5745033b59475d50de0a42ae40e9336
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-06-17 11:40:28 +0700

    chore: added sharedgoal model and migration / factory

---
+++ database/factories/SharedGoalFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Goal;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SharedGoal>
+ */
+class SharedGoalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'goal_id' => Goal::factory(),
+        ];
+    }
+}
